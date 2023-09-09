374cd799ca3b1996901a2921120cb572d738efa8
oleksandr-pavliuk <61949378+FoldeR-ua@users.noreply.github.com>
2023-09-09 12:50:56 +0300

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿var singleton = SingletonClass.GetInstance();
+
+
+public class SingletonClass
+{
+    public static SingletonClass instance;      // storing the static instance of this class
+    
+    private SingletonClass(){}             // creating the private constructor that forbid to create the object outside this class
+
+    public static SingletonClass GetInstance()   // static method for getting this one instance
+    {
+        if (instance == null)
+            instance = new SingletonClass();
+        return instance;
+    }
+}
