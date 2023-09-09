017a1d09454c0e8816d0608c60a564f7b3428db6
oleksandr-pavliuk <61949378+FoldeR-ua@users.noreply.github.com>
2023-09-09 13:15:33 +0300

    Adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,59 @@
+﻿var human = new Human();
+var auto = new Auto();
+human.Travel(auto);
+var plane = new Plane();
+human.Travel(new PlaneToDriver(plane));
+
+public class Human
+{
+    public void Travel(IDriver driver)
+    {
+        driver.Drive();
+    }
+}
+public interface IDriver
+{
+    void Drive();
+}
+class Auto : IDriver
+{
+    public void Drive()
+    {
+        Console.WriteLine("Auto move to the destination");
+    }
+}
+class Driver
+{
+    public void Travel(IDriver transport)
+    {
+        transport.Drive();
+    }
+}
+
+interface IFlight
+{
+    void Fly();
+}
+
+class Plane : IFlight
+{
+    public void Fly()
+    {
+        Console.WriteLine("Plane flies to the destination");
+    }
+}
+
+class PlaneToDriver : IDriver
+{
+    private Plane plane;
+
+    public PlaneToDriver(Plane plane)
+    {
+        this.plane = plane;
+    }
+
+    public void Drive()
+    {
+        plane.Fly();
+    }
+}
